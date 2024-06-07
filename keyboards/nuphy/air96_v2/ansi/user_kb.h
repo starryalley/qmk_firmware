7c1ff5539b9c1f815a08f4e2bc741a58ed7cc02c
adi4086 <adi4086>
2024-06-07 16:09:07 +0300

    space

--- keyboards/nuphy/air96_v2/ansi/user_kb.h
+++ keyboards/nuphy/air96_v2/ansi/user_kb.h
@@ -41,86 +41,83 @@ typedef enum {
 } TYPE_RX_STATE;
 
 
-#define RF_IDLE                 0
-#define RF_PAIRING              1
-#define RF_LINKING              2
-#define RF_CONNECT              3
-#define RF_DISCONNECT           4
-#define RF_SLEEP                5
-#define RF_SNIF                 6
-#define RF_INVAILD              0XFE
-#define RF_ERR_STATE            0XFF
+#define RF_IDLE                   0
+#define RF_PAIRING                1
+#define RF_LINKING                2
+#define RF_CONNECT                3
+#define RF_DISCONNECT             4
+#define RF_SLEEP                  5
+#define RF_SNIF                   6
+#define RF_INVAILD                0XFE
+#define RF_ERR_STATE              0XFF
 
-#define CMD_POWER_UP            0XF0
-#define CMD_SLEEP               0XF1
-#define CMD_HAND                0XF2
-#define CMD_SNIF                0XF3
-#define CMD_24G_SUSPEND         0XF4
-#define CMD_IDLE_EXIT           0XFE
+#define CMD_POWER_UP              0XF0
+#define CMD_SLEEP                 0XF1
+#define CMD_HAND                  0XF2
+#define CMD_SNIF                  0XF3
+#define CMD_24G_SUSPEND           0XF4
+#define CMD_IDLE_EXIT             0XFE
 
-#define CMD_RPT_MS              0XE0
-#define CMD_RPT_BYTE_KB         0XE1
-#define CMD_RPT_BIT_KB          0XE2
-#define CMD_RPT_CONSUME         0XE3
-#define CMD_RPT_SYS             0XE4
+#define CMD_RPT_MS                0XE0
+#define CMD_RPT_BYTE_KB           0XE1
+#define CMD_RPT_BIT_KB            0XE2
+#define CMD_RPT_CONSUME           0XE3
+#define CMD_RPT_SYS               0XE4
 
-#define CMD_SET_LINK            0XC0
-#define CMD_SET_CONFIG          0XC1
-#define CMD_GET_CONFIG          0XC2
-#define CMD_SET_NAME            0XC3
-#define CMD_GET_NAME            0XC4
-#define CMD_CLR_DEVICE          0XC5
-#define CMD_NEW_ADV             0XC7
-#define CMD_RF_STS_SYSC         0XC9
-#define CMD_SET_24G_NAME        0XCA
-#define CMD_GO_TEST             0XCF
-#define CMD_RF_DFU              0XB1
-#define CMD_NULL                0X00
+#define CMD_SET_LINK              0XC0
+#define CMD_SET_CONFIG            0XC1
+#define CMD_GET_CONFIG            0XC2
+#define CMD_SET_NAME              0XC3
+#define CMD_GET_NAME              0XC4
+#define CMD_CLR_DEVICE            0XC5
+#define CMD_NEW_ADV               0XC7
+#define CMD_RF_STS_SYSC           0XC9
+#define CMD_SET_24G_NAME          0XCA
+#define CMD_GO_TEST               0XCF
+#define CMD_RF_DFU                0XB1
+#define CMD_NULL                  0X00
 
-#define CMD_WRITE_DATA          0X80
-#define CMD_READ_DATA           0X81
+#define CMD_WRITE_DATA            0X80
+#define CMD_READ_DATA             0X81
 
-#define LINK_RF_24              0
-#define LINK_BT_1               1
-#define LINK_BT_2               2
-#define LINK_BT_3               3
-#define LINK_USB                4
+#define LINK_RF_24                0
+#define LINK_BT_1                 1
+#define LINK_BT_2                 2
+#define LINK_BT_3                 3
+#define LINK_USB                  4
 
-#define UART_HEAD               0x5A
-#define FUNC_VALID_LEN          32
-#define UART_MAX_LEN            64
+#define UART_HEAD                 0x5A
+#define FUNC_VALID_LEN            32
+#define UART_MAX_LEN              64
 
-#define SYS_SW_WIN              0xa1
-#define SYS_SW_MAC              0xa2
+#define SYS_SW_WIN                0xa1
+#define SYS_SW_MAC                0xa2
 
-#define RF_LINK_SHOW_TIME       300
+#define RF_LINK_SHOW_TIME         300
 
-#define HOST_USB_TYPE           0
-#define HOST_BLE_TYPE           1
-#define HOST_RF_TYPE            2
+#define HOST_USB_TYPE             0
+#define HOST_BLE_TYPE             1
+#define HOST_RF_TYPE              2
 
-#define RF_POWER_DOWN_DELAY     (30)
+#define RF_POWER_DOWN_DELAY       (30)
 
-// #define RF_LONG_PRESS_DELAY     30
-// #define DEV_RESET_PRESS_DELAY   30
-// #define RGB_TEST_PRESS_DELAY    30
-#define MICRO_PRESS_DELAY       5
-#define SMALL_PRESS_DELAY       10
-#define MEDIUM_PRESS_DELAY      30
-#define LONG_PRESS_DELAY        50
+#define MICRO_PRESS_DELAY         5
+#define SMALL_PRESS_DELAY         10
+#define MEDIUM_PRESS_DELAY        30
+#define LONG_PRESS_DELAY          50
 
-#define NO_ACT_TIME_MINUTE      (60 * 100)
+#define NO_ACT_TIME_MINUTE        (60 * 100)
 
-#define RGB_MATRIX_GAME_MODE                RGB_MATRIX_GRADIENT_LEFT_RIGHT
-#define RGB_MATRIX_GAME_MODE_VAL            RGB_MATRIX_MAXIMUM_BRIGHTNESS - RGB_MATRIX_VAL_STEP * 2
-#define SIDE_MATRIX_GAME_MODE               4
+#define RGB_MATRIX_GAME_MODE      RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#define RGB_MATRIX_GAME_MODE_VAL  RGB_MATRIX_MAXIMUM_BRIGHTNESS - RGB_MATRIX_VAL_STEP * 2
+#define SIDE_MATRIX_GAME_MODE     4
 
-#define    CAPS_LED            55
-#define    LSHIFT_LED          71
-#define    WIN_LED             89
-#define    NUMLOCK_LED         33
+#define    CAPS_LED               55
+#define    LSHIFT_LED             71
+#define    WIN_LED                89
+#define    NUMLOCK_LED            33
 
-#define USB_ACTIVE              ((dev_info.link_mode == LINK_USB && USB_DRIVER.state != USB_SUSPENDED) || (dev_info.link_mode != LINK_USB && dev_info.rf_charge == 0x03))
+#define USB_ACTIVE                ((dev_info.link_mode == LINK_USB && USB_DRIVER.state != USB_SUSPENDED) || (dev_info.link_mode != LINK_USB && dev_info.rf_charge == 0x03))
 
 typedef struct
 {
