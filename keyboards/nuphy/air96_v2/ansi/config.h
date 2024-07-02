ff66d5f83344208796ca05b17bcad5ccff5f72b7
adi4086 <adi4086>
2024-07-02 15:02:40 +0300

    readjust pin delay

--- keyboards/nuphy/air96_v2/ansi/config.h
+++ keyboards/nuphy/air96_v2/ansi/config.h
@@ -81,7 +81,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define EECONFIG_KB_DATA_SIZE               12
 #define MCU_SLEEP_ENABLE                    1 
-#define GPIO_INPUT_PIN_DELAY                6
+#define GPIO_INPUT_PIN_DELAY                12
 #define RF_QUEUE_SIZE                       64
 
 #define RGB_MATRIX_DISABLE_KEYCODES
